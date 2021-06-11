8a25a08dd3629bfe9ff8e3d289b8aaf258baff38
Brian Peterson <bpeterson1579@gmail.com>
2021-06-11 11:48:14 -0600

    Initial commit

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  name: 'T-Rex',
+  eraLived: 'Late Cretaceous',
+  heightInFeet: 12,
+  weightInLbs: 9900,
+  isExtinct: true,
+  isFlying: false,
+  movieAppearances: ["A Dinosaur's Story", "Toy Story", "Jurassic Park"],
+  fossilLocations: ["Montana", "Texas", "Utah", "Wyoming"]
+}
