797a935bfd104a068153d28354bb40de83ef01ad
Brian Peterson <bpeterson1579@gmail.com>
2021-06-11 11:53:31 -0600

    Initial commit

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  firstName: 'Brian',
+  lastName: 'Peterson',
+  age: 30,
+  heightInInches: 71,
+  isTesting: true,
+  isStressed: true,
+  placesLived: ['Arvada,CO', 'Greeley,CO', 'Denver,CO'],
+  sportsPlayed: ['Soccer', 'Football', 'Hockey', 'Lacrosse', 'Bowling'],
+}
